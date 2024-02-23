ccb5854b39649164149f93c5f41dd7ca50c62462
dhanisaptrri <shadhanisa2@gmail.com>
2024-02-23 14:23:03 +0700

    membetulkan yang salah

--- Jobsheet2/BukuMain07.java
+++ Jobsheet2/BukuMain07.java
@@ -1,17 +1,17 @@
 public class BukuMain07 {
     
    public static void main(String [] args) {
-        //Buku07 bk1 = new Buku07();
-        //bk1.judul = "Today Ends Tomorrow Comes";
-        //bk1. pengarang = "Denanda Pratiwi";
-        //bk1.halaman = 198;
-        //bk1.stok = 13;
-        //bk1.harga = 7100;
+        Buku07 bk1 = new Buku07();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1. pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 7100;
 
-        //bk1.tampilInformasi();
-        //bk1.terjual(5);
-        //bk1.gantiHarga(6000);
-        //bk1.tampilInformasi();
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(6000);
+        bk1.tampilInformasi();
 
         Buku07 bk2 = new Buku07("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
