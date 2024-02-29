c5383997b908ba1a00225e907b15af1339eeebee
dhanisaptrri <shadhanisa2@gmail.com>
2024-02-29 19:18:54 +0700

    Percobaan 1

---
+++ Jobsheet3/ArrayObject07.java
@@ -0,0 +1,24 @@
+public class ArrayObject07 {
+
+    public static void main(String [] args) {
+        PersegiPanjang07[] ppArray = new PersegiPanjang07[3];
+
+        ppArray[0] = new PersegiPanjang07();
+        ppArray[0].panjang = 110;
+        ppArray[0].lebar = 30;
+
+        ppArray[1] = new PersegiPanjang07();
+        ppArray[1].panjang = 80;
+        ppArray[1].lebar = 40;
+
+        ppArray[2] = new PersegiPanjang07();
+        ppArray[2].panjang = 100;
+        ppArray[2].lebar = 20;
+
+        System.out.println("Persegi Panjang ke-0, panjang : " + ppArray[0].panjang + ", lebar : " + ppArray[0].lebar);
+        System.out.println("Persegi Panjang ke-0, panjang : " + ppArray[1].panjang + ", lebar : " + ppArray[1].lebar);
+        System.out.println("Persegi Panjang ke-0, panjang : " + ppArray[2].panjang + ", lebar : " + ppArray[2].lebar);
+        
+    }
+    
+}
---
+++ Jobsheet3/PersegiPanjang07.java
@@ -0,0 +1,6 @@
+public class PersegiPanjang07 {
+
+    public int panjang;
+    public int lebar;
+    
+}
