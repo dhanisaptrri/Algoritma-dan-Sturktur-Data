5705ff68a43bc0ed110c5f1cdc1bbe016d561348
dhanisaptrri <shadhanisa2@gmail.com>
2024-02-29 23:08:30 +0700

    Percobaan 3

---
+++ Jobsheet3/ArrayBalok07.java
@@ -0,0 +1,16 @@
+public class ArrayBalok07 {
+
+    public static void main(String[] args) {
+
+        Balok07[] blArray = new Balok07[3];
+
+        blArray[0] = new Balok07(100, 30, 12);
+        blArray[1] = new Balok07(120, 40, 15);
+        blArray[2] = new Balok07(210, 50, 25);
+
+        for(int i = 0; i < 3; i++) {
+
+            System.out.println("Volume balok ke " + i + " : " + blArray[i].hitungVolume());
+        }
+    }
+}
