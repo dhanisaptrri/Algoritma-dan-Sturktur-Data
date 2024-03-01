1257f283f162f806545176af184cdc527ce772bf
dhanisaptrri <shadhanisa2@gmail.com>
2024-03-01 09:04:30 +0700

    Praktikum2

---
+++ Jobsheet3/Praktikum2/dataMhs.java
@@ -0,0 +1,12 @@
+package Praktikum2;
+
+public class dataMhs {
+
+    public String nama;
+    public int nim;
+    public String jnsKelamin;
+    public double ipk;
+
+    
+    
+}
---
+++ Jobsheet3/Praktikum2/inputDataMhs.java
@@ -0,0 +1,40 @@
+package Praktikum2;
+
+import java.util.Scanner;
+
+public class inputDataMhs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        dataMhs[] inputData = new dataMhs[3];
+
+        for(int i = 0; i < inputData.length; i++){
+            inputData[i] = new dataMhs();
+            System.out.println("Masukan Data Mahasiswa ke-" + (i + 1));
+
+            System.out.print("Masukan Nama Mahasiswa : ");
+            inputData[i].nama = sc.next();
+
+            System.out.print("Masukan NIM : ");
+            inputData[i].nim = sc.nextInt();
+            
+            System.out.print("Masukan Jenis Kelamin : ");
+            inputData[i].jnsKelamin = sc.next();
+
+            System.out.print("Masukan IPK : ");
+            inputData[i].ipk = sc.nextDouble();
+
+        }
+
+        for(int i = 0; i < inputData.length; i++){
+            System.out.println("Data Mahasiswa ke-" + (i+1) );
+            System.out.println("Nama            : " + inputData[i].nama);
+            System.out.println("Nim             : " + inputData[i].nim);
+            System.out.println("Jenis Kelamin   : " + inputData[i].jnsKelamin);
+            System.out.println("IPK             : " +inputData[i].ipk);
+        }
+
+    }
+    
+}
